02b8b4fd0d92cfc27d7b0432db7b5e84cd2f02a6
SofijaVvv <137697302+SofijaVvv@users.noreply.github.com>
2024-03-12 17:17:58 +0100

    MS 1

---
+++ src/services/otp_service.ts
@@ -0,0 +1,24 @@
+import speakeasy from 'speakeasy';
+
+
+export async function generateOTPSecret(){
+    return speakeasy.generateSecret({length: 12});
+}
+
+export async function generateOtpQRCode(secret:string, userEmail:string){
+    const appName = "LgProject";
+    const otpAuthUrl = `otpauth://totp/${appName}:${userEmail}?secret=${secret}&issuer=${appName}`;
+    return  `https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=${otpAuthUrl}`;
+}
+
+export async function verifyUserOTP(OTP:string, userSecret:string){
+    return speakeasy.totp.verify({
+        secret: userSecret,
+        encoding: 'base32',
+        token: OTP,
+        window: 6
+    });
+}
+
+// - kod upisa opretera (generateOTPSecret(), genetateOtpQRCode())
+// - u login za provjeru otp verify
